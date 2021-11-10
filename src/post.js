13ceda6304d1751b7e93df0a88cc7c8aa44a90d6
Rvvse <electron1c1019@163.com>
2021-11-10 14:45:17 +0900

    Update post.js

--- src/post.js
+++ src/post.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './post.css';
 import ReactDOM from 'react-dom';
 
+function Post(props) {
+    return ();
+}
 
-
-
+const element = <Post />;
 
 ReactDOM.render(element, document.getElementById('root'));
